40264b753c5089e755f673c5bef3197c5c575e04
balfoud <balfour.d@northeastern.edu>
2023-10-19 13:47:18 -0400

    started Deck class

--- Project2/Deck.cpp
+++ Project2/Deck.cpp
@@ -1,1 +1,16 @@
+// Group 11: Drew Balfour and Sean Vumbaco
+// Project 2 (parts a and b)
 
+// Contains member functions for class Deck. Including:
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "Deck.h"
+
+Deck::Deck()
+// default class Deck constructor
+{
+    
+}
--- Project2/Deck.h
+++ Project2/Deck.h
@@ -1,1 +1,28 @@
+// Group 11: Drew Balfour and Sean Vumbaco
+// Project 2 (parts a and b)
 
+// Contains class Deck declaration for Flip
+
+#ifndef DECK_H
+#define DECK_H
+
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+class Deck 
+{
+    private:
+        // member data
+        node<Card> *first;
+    public:
+        // constructor
+        Deck();
+        // member functions
+        void shuffle();
+        friend ostream& operator<< (ostream& out, Deck& deck)
+        
+};
+
+#endif //DECK_H
