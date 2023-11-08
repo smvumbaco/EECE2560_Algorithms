36deaf0333f3357b4c5e4d23bce416e06477a3ac
Sean V <vumbaco.s@northeastern.edu>
2023-11-08 11:19:20 -0500

    Added definitions in dictionary.h file and added file with all dictionary words

--- Project 3/dictionary.h
+++ Project 3/dictionary.h
@@ -1,1 +1,24 @@
+// Group 11: Drew Balfour & Sean Vumbaco
+// Project 3 (parts a and b)
 
+// Contains class Dictionary declaration
+
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Dictionary {
+    private:
+        vector<string> dictionary;
+    public:
+        Dictionary();
+        void printDictionary();
+        void selectionSortDictionary();
+        string binarySearch(string word);
+};
+
+#endif
